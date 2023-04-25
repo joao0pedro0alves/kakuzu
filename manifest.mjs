a2a7602f6e645c0b81f086d4b459f0aadf2f609f
João Alves <joao.alves@ecosis.com.br>
2023-04-25 16:53:41 -0300

    chore: adjust web manifest

--- manifest.mjs
+++ manifest.mjs
@@ -4,12 +4,12 @@ export default {
   description: '',
   icons: [
     {
-      src: 'images/favicon/android-chrome-192x192.png',
+      src: 'images/android-chrome-192x192.png',
       sizes: '192x192',
       type: 'image/png',
     },
     {
-      src: 'images/favicon/android-chrome-512x512.png',
+      src: 'images/android-chrome-512x512.png',
       sizes: '512x512',
       type: 'image/png',
     },
