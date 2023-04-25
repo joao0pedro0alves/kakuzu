aa011c1b81c8cbb8793d1eb4ad94dd9206844c82
João Alves <joao.alves@ecosis.com.br>
2023-04-25 14:09:47 -0300

    chore: update assets

--- manifest.mjs
+++ manifest.mjs
@@ -9,16 +9,5 @@ export default {
       sizes: '192x192',
       type: 'image/png'
     },
-    {
-      src: 'images/favicon/pwa-512x512.png',
-      sizes: '512x512',
-      type: 'image/png'
-    },
-    {
-      src: 'images/favicon/pwa-512x512.png',
-      sizes: '512x512',
-      type: 'image/png',
-      purpose: 'any maskable'
-    }
   ]
 }
