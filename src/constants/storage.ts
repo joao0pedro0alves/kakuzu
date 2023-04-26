a53685a0e7a2d2d50dd2712dd2fcaf2c3545749b
João Alves <joao.alves@ecosis.com.br>
2023-04-26 09:34:58 -0300

    chore: rename folder `services` to `utils`

--- src/constants/storage.ts
+++ src/constants/storage.ts
@@ -1,3 +1,4 @@
 export const TRANSACTIONS = '@kakuzu:app-transactions'
 export const THEME = '@kakuzu:app-theme'
 export const VISIBLE = '@kakuzu:app-visible'
+export const CALCULATOR_INCLUDED_LISTS = '@kakuzu:app-active-lists'
