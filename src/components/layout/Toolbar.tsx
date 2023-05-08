98a0442a5930c139862a2b873ceb67d741bae6a9
João Alves <joao.alves@ecosis.com.br>
2023-05-08 15:41:48 -0300

    chore: create drawer component

--- src/components/layout/Toolbar.tsx
+++ src/components/layout/Toolbar.tsx
@@ -1,9 +1,19 @@
+import { BiMenu } from 'react-icons/bi'
 import { ThemeSwitcher } from '../ThemeSwitcher'
 import { VisibleSwitcher } from '../VisibleSwitcher'
 
-export function Toolbar() {
+interface ToolbarProps {
+  onOpen: () => void
+}
+
+export function Toolbar({ onOpen }: ToolbarProps) {
   return (
-    <header className="flex justify-end items-center px-4 py-4 container mx-auto">
+    <header className="flex justify-between lg:justify-end items-center px-4 py-4 container mx-auto">
+
+      <button className='hidden max-lg:block' onClick={onOpen}>
+        <BiMenu size={24} />
+      </button>
+
       <div className="flex items-center gap-4">
         <VisibleSwitcher />
         <ThemeSwitcher />
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 
 import { Sidebar } from '@/components/layout/Sidebar'
@@ -7,14 +8,21 @@ import { VisibleProvider } from '@/contexts/Visible'
 import { TransactionsProvider } from '@/contexts/Transactions'
 
 export function MainPage() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <VisibleProvider>
       <TransactionsProvider>
         <main className="w-screen h-screen flex flex-col items-center overflow-x-hidden">
-          <Toolbar />
+          <Toolbar 
+            onOpen={() => setIsOpen(true)}
+          />
 
           <div className="container relative flex-1 md:max-h-[90%] flex gap-4">
-            <Sidebar />
+            <Sidebar 
+              isOpen={isOpen}
+              onClose={() => setIsOpen(false)}
+            />
 
             <section className="flex-1 max-md:shadow-inner md:flex-[4] bg-white p-4 rounded shadow-sm dark:bg-gray-800">
               <Outlet />
