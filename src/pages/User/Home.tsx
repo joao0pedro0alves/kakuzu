feafb80f3a9f44702eecca85ed6c242819a9a2ea
João Alves <joao.alves@ecosis.com.br>
2023-05-08 14:47:53 -0300

    chore: mobile response adjust

--- src/pages/User/Home.tsx
+++ src/pages/User/Home.tsx
@@ -31,20 +31,20 @@ export function HomePage() {
 
   return (
     <section>
-      <div className="grid grid-cols-3 gap-4 mb-4">
-        <div className="p-4 flex flex-col items-center justify-center shadow rounded bg-gray-100 dark:bg-gray-700/50">
+      <div className="grid grid-cols-3 max-md:grid-rows-3 grid-flow-col md:grid-flow-row gap-4 mb-4">
+        <div className="max-md:col-span-3 p-4 flex flex-col items-center justify-center shadow rounded bg-gray-100 dark:bg-gray-700/50">
           <span className="text-sm text-left text-gray-700 dark:text-gray-300">Saldo atual</span>
           <span className={clsx("font-bold text-3xl", dailyBalance > 0 ? 'text-green-500' : 'text-red-500')}>
             <VisibleText value={currencyFormatter(dailyBalance)} />
           </span>
         </div>
-        <div className="p-4 flex flex-col items-center justify-center shadow rounded bg-gray-100 dark:bg-gray-700/50">
+        <div className="max-md:col-span-3 p-4 flex flex-col items-center justify-center shadow rounded bg-gray-100 dark:bg-gray-700/50">
           <span className="text-sm text-left text-gray-700 dark:text-gray-300">Saldo agendado</span>
           <span className={clsx("font-bold text-3xl", dailyBalance > 0 ? 'text-green-500' : 'text-red-500')}>
             <VisibleText value={currencyFormatter(scheduledBalance)} />
           </span>
         </div>
-        <div className="p-4 flex flex-col items-center justify-center shadow rounded bg-gray-100 dark:bg-gray-700/50">
+        <div className="max-md:col-span-3 p-4 flex flex-col items-center justify-center shadow rounded bg-gray-100 dark:bg-gray-700/50">
           <span className="text-sm text-left text-gray-700 dark:text-gray-300">Dia com mais saídas</span>
           <span className="font-bold text-3xl text-red-500">{dateFormatter(getDateWithMoreExpenses())}</span>
         </div>
